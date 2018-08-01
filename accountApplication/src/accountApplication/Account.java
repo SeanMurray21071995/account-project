78742ab0680d378d4619811c5870aafa8fcba3bc
Sean <sean.murray@academytrainee.com>
2018-08-01 08:34:55 +0100

    This is the half finished account thing

---
+++ accountApplication/src/accountApplication/Account.java
@@ -0,0 +1,37 @@
+package accountApplication;
+
+public class Account {
+	private String first_name; 
+	private String second_name; 
+	private int accountNumber;
+	
+	public Account (String first_name, String second_name, int accountNumber) 
+	{
+		
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getSecond_name() {
+		return second_name;
+	}
+
+	public void setSecond_name(String second_name) {
+		this.second_name = second_name;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
+}
---
+++ accountApplication/src/accountApplication/Service.java
@@ -0,0 +1,17 @@
+package accountApplication;
+
+import java.util.HashMap;
+
+public class Service {
+	
+	private HashMap<Integer, Account> accounts = new HashMap<Integer, Account>();
+	
+	public void addAccount(Account acc) 
+	{	
+		accounts.put(acc.getAccountNumber(), acc);
+	}
+	public Account getAccount(int accountNumber) 
+	{
+		return accounts.get(accountNumber);
+	}
+}
