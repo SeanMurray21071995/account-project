2c29f60d3440fd1199a54fa4e42d79d04d644473
Sean <sean.murray@academytrainee.com>
2018-08-01 17:17:33 +0100

    new function to read in a name and count how many instances there are in the hash map

--- accountApplication/src/accountApplication/Service.java
+++ accountApplication/src/accountApplication/Service.java
@@ -23,13 +23,28 @@ public class Service {
 	public JSONObject jobj() 
 	{
 		Iterator it = accounts.entrySet().iterator();
-		JSONObject accountReturn = new JSONObject();
-		while (it.hasNext()) {
+		JSONObject accountReturn = new JSONObject(accounts);
+	/*	while (it.hasNext()) {
 	        Map.Entry pair = (Map.Entry)it.next();
 	        System.out.println(pair.getKey() + " = " + pair.getValue());
 	        accountReturn.put( pair.getKey().toString(), ((Account) pair.getValue()).toJSONObject());
-		}
-		
+		}*/
 		return accountReturn;	
 	}
+	 
+	public int getFirstNameCount(String lookForName) 
+	{
+		int count = 0;
+		Iterator it = accounts.entrySet().iterator();
+		while(it.hasNext()) 
+		{
+			Map.Entry entry = (Map.Entry)it.next();
+		 	if((((Account)entry.getValue()).getFirstName()).equals(lookForName)) 
+			{ 
+				count = count +1;
+			}
+		}
+		return count;
+	}
+	
 }
--- accountApplication/src/testAccountApplication/TestService.java
+++ accountApplication/src/testAccountApplication/TestService.java
@@ -2,18 +2,32 @@ package testAccountApplication;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import accountApplication.Account;
 import accountApplication.Service;
 
 public class TestService {
+	private Service serv = new Service();
+	private Account acc = new Account("test","test", 1);
+	@Before
+	public void setup() 
+	{
+		serv.addAccount(acc);
+	}
 	@Test 
 	public void getAddAccount() 
 	{
-		Service serv = new Service();
-		Account acc = new Account("test","test", 1);
 		serv.addAccount(acc);
 		assertEquals("did not retriev the data from the hash map", serv.getAccount(1),acc);
 	}
+
+
+	@Test
+	public void testTestFirstNameCount() 
+	{
+		
+		assertEquals("didn't get the correct count",serv.getFirstNameCount("test"),1);
+	} 
 }
